b5d6913e24faaaee5a75d43f659842c51989bf56
Jordan Rodrigues <96402459+j0rdanR@users.noreply.github.com>
2021-12-21 22:47:35 +0800

    added mobile ui panels
    navbar and header

--- voidpath/src/app/app-routing.module.ts
+++ voidpath/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { BodyComponent } from './components/body/body.component';
+import { AccountComponent } from './components/pages/account/account.component';
 import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
 import { Error404Component } from './components/pages/error404/error404.component';
 import { HomeComponent } from './components/pages/home/home.component';
@@ -11,11 +12,13 @@ const routes: Routes = [
   { path: '', component: BodyComponent,
     children: [
       { path: '', component: HomeComponent },
-      { path: 'dashboard', component: DashboardComponent }
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'account', component: AccountComponent },
+      
+      { path: '**', component: Error404Component },
     ]
   },
 
-  { path: '**', component: Error404Component }
   
 ];
 
