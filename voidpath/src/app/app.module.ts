c6b03b1b324e9cc998b6a11f98bcc6674b76e3bd
Jordan Rodrigues <96402459+j0rdanR@users.noreply.github.com>
2021-12-21 18:49:34 +0800

    generate main components
    using angular cli

--- voidpath/src/app/app.module.ts
+++ voidpath/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './components/body/body.component';
+import { HeaderComponent } from './components/body/header/header.component';
+import { DesktopNavComponent } from './components/body/desktop-nav/desktop-nav.component';
+import { MobileNavComponent } from './components/body/mobile-nav/mobile-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    HeaderComponent,
+    DesktopNavComponent,
+    MobileNavComponent
   ],
   imports: [
     BrowserModule,
---
+++ voidpath/src/app/components/body/desktop-nav/desktop-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-desktop-nav',
+  templateUrl: './desktop-nav.component.html',
+  styleUrls: ['./desktop-nav.component.scss']
+})
+export class DesktopNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
