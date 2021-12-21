d48727f67e47c0a5001a019ad945b565aab6e01e
Jordan Rodrigues <96402459+j0rdanR@users.noreply.github.com>
2021-12-21 20:47:21 +0800

    configured routing
    additional pages added

--- voidpath/src/app/app-routing.module.ts
+++ voidpath/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { BodyComponent } from './components/body/body.component';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { Error404Component } from './components/pages/error404/error404.component';
+import { HomeComponent } from './components/pages/home/home.component';
+
+const routes: Routes = [
+
+  { path: '', component: BodyComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'dashboard', component: DashboardComponent }
+    ]
+  },
+
+  { path: '**', component: Error404Component }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- voidpath/src/app/app.module.ts
+++ voidpath/src/app/app.module.ts
@@ -7,6 +7,12 @@ import { BodyComponent } from './components/body/body.component';
 import { HeaderComponent } from './components/body/header/header.component';
 import { DesktopNavComponent } from './components/body/desktop-nav/desktop-nav.component';
 import { MobileNavComponent } from './components/body/mobile-nav/mobile-nav.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { AccountComponent } from './components/pages/account/account.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { RegisterComponent } from './components/pages/register/register.component';
+import { Error404Component } from './components/pages/error404/error404.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +20,13 @@ import { MobileNavComponent } from './components/body/mobile-nav/mobile-nav.comp
     BodyComponent,
     HeaderComponent,
     DesktopNavComponent,
-    MobileNavComponent
+    MobileNavComponent,
+    HomeComponent,
+    DashboardComponent,
+    AccountComponent,
+    LoginComponent,
+    RegisterComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
