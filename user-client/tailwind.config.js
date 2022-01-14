a7e39c3c72df144ff9a794530a3787f305e16ab0
Jordan Rodrigues <96402459+j0rdanR@users.noreply.github.com>
2022-01-14 18:20:58 +0800

    reset branch
    lost some stuff, please forgive me for what im about to commit lol

---
+++ user-client/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    prefix: '',
+    purge: {
+      content: [
+        './src/**/*.{html,ts}',
+      ]
+    },
+    darkMode: 'class',
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    }
+};
