52d66c2d5d491562fbe5d19059a4a1a00cab3dd7
Jordan Rodrigues <96402459+j0rdanR@users.noreply.github.com>
2022-01-17 16:10:26 +0800

    added main pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AccountComponent } from './pages/account/account.component';
 import { SettingsComponent } from './pages/settings/settings.component';
+import { AppsComponent } from './pages/apps/apps.component';
+import { BrowseComponent } from './pages/browse/browse.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'apps', component: AppsComponent },
+  { path: 'browse', component: BrowseComponent },
   { path: 'settings', component: SettingsComponent },
   { path: 'account', component: AccountComponent },
 ];
